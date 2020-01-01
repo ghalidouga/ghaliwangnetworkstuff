17d6f4e2ac355f62fc7cd41585bf5e06dd4d964f
ghalidouga <ghalighalidouga@gmail.com>
2020-01-01 23:08:46 +0700

    hook login event authme

--- src/main/java/Events/Login.java
+++ src/main/java/Events/Login.java
@@ -1,15 +1,23 @@
 package Events;
 
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.potion.PotionEffectType;
 
 public class Login implements Listener {
     @EventHandler
     public void onLogin (fr.xephi.authme.events.LoginEvent e){
         Player player = e.getPlayer();
-        
+        Location overWorld = new Location(Bukkit.getWorld("world"),0,100,0,0,0);
+        player.teleport(overWorld);
+
+
+
 
     }
 }
--- src/main/java/Events/OnJoin.java
+++ src/main/java/Events/OnJoin.java
@@ -1,18 +1,27 @@
 package Events;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class OnJoin implements Listener {
     @EventHandler
-    public void  onJoin (PlayerJoinEvent e){
+    public void onJoin(PlayerJoinEvent e) {
         Player player = e.getPlayer();
-        Location flatLogin = new Location(Bukkit.getWorld("flatLogin"),0.5,5,0.5,90,0);
+        Location flatLogin = new Location(Bukkit.getWorld("flatLogin"), 0.5, 5, 0.5, 90, 0);
         player.teleport(flatLogin);
-
     }
 }
+
+
+
+
+
+
